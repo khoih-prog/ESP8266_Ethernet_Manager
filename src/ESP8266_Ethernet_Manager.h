84985a1e67b38dbadaf87fc1f05e80227010e2de
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2022-12-12 23:34:37 -0500

    v1.0.0 for `ESP8266 + LwIP W5x00 / ENC28J60`
    #### Releases v1.0.0
    
    1. Initial coding to port `synchronous` [ESP_WiFiManager](https://github.com/khoih-prog/ESP_WiFiManager) to ESP8266 boards using `LwIP W5500 / W5100(S) / ENC28J60 Ethernet`.
    2. Use `allman astyle`

---
+++ src/ESP8266_Ethernet_Manager.h
@@ -0,0 +1,30 @@
+/****************************************************************************************************************************
+  ESP8266_Ethernet_Manager.h
+
+  For Ethernet shields using ESP8266 Ethernet (ESP8266 + LwIP W5500 / W5100(S) / ENC28J60)
+
+  Modified from
+  1. Tzapu               (https://github.com/tzapu/WiFiManager)
+  2. Ken Taylor          (https://github.com/kentaylor)
+  3. Khoi Hoang          (https://github.com/khoih-prog/ESP_WiFiManager)
+
+  Built by Khoi Hoang https://github.com/khoih-prog/ESP8266_Ethernet_Manager
+  Licensed under MIT license
+
+  Version: 1.0.0
+
+  Version Modified By  Date      Comments
+  ------- -----------  ---------- -----------
+  1.0.0   K Hoang     13/12/2022 Initial coding for (ESP8266 + LwIP W5500 / W5100(S) / ENC28J60)
+ *****************************************************************************************************************************/
+
+#pragma once
+
+#ifndef ESP8266_Ethernet_Manager_h
+#define ESP8266_Ethernet_Manager_h
+
+#include <ESP8266_Ethernet_Manager.hpp>              //https://github.com/khoih-prog/ESP8266_Ethernet_Manager
+#include <ESP8266_Ethernet_Manager_Impl.h>           //https://github.com/khoih-prog/ESP8266_Ethernet_Manager
+
+#endif    // ESP8266_Ethernet_Manager_h
+
